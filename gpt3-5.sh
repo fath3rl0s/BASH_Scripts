65348d7ed10c2c2c59ede8b5cc649276d31abcc5
Carlos Enamorado <88733190+fath3rl0s@users.noreply.github.com>
2023-06-25 21:35:01 -0400

    Update gpt3-5.sh

--- gpt3-5.sh
+++ gpt3-5.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 #Prompt script for ChatGPT
 #Carlos Enamorado
-#
-#
-#
-#Model Enpoint Compatibility
-#https://platform.openai.com/docs/models/overview
-#
-#
+'''
+This script leverages ChatGPT through the command line. 
+A subscription is recommended.
+
+Engine Model can be subsituted although your curl request may need to be modified
+
+Consult OpenAI:
+Model Enpoint Compatibility
+https://platform.openai.com/docs/models/overview
+'''
 
 #Set API Key
-API_Key="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"
+API_Key="<ENTER YOUR UNIQUE API KEY>"
 
 
 #Function to interact with ChatGPT
 function chat_gpt() {
 	local prompt="$1"
+ 	#OpenAI uses json formatting for requests
 	local response=$(curl -s -X POST \
 		-H "Content-Type: application/json" \
 		-H "Authorization: Bearer $API_Key" \
 		-d "{\"messages\": [{\"role\": \"system\", \"content\": \"$prompt\"}], \"model\": \"gpt-3.5-turbo\"}" \
 		"https://api.openai.com/v1/chat/completions")
-	
-
 	local answer=$(echo "$response")
-	echo "$answer"
-       
+	echo "$answer"      
 }
-
 
 #Main script that runs in an infinite loop
 while true; do
